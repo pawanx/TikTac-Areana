756bb03da230f4a4c92e38ed8fca2a14cafb3e45
Pawan <pawanmishra196@gmail.com>
2026-06-19 09:43:05 +0530

    feat: game room updated

--- client/src/pages/GameRoom/GameRoom.jsx
+++ client/src/pages/GameRoom/GameRoom.jsx
@@ -1,35 +1,177 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { socket } from "../../socket/socket";
+import Confetti from "react-confetti";
+import { useWindowSize } from "@uidotdev/usehooks";
+import { useNavigate } from "react-router-dom";
+
+import { useAuth } from "../../contexts/AuthContext";
 import Board from "../../components/Board/Board";
+import "../GameRoom/GameRoom.css";
+import winner_sound from "../../assets/sounds/winner_sound.mp3";
 
 const GameRoom = () => {
+  const winnerAudio = new Audio(winner_sound);
+  const navigate = useNavigate();
+  const { width, height } = useWindowSize();
+  const { user } = useAuth();
   const location = useLocation();
+  const { roomCode } = useParams();
 
-  const room = location.state;
+  const [room, setRoom] = useState(location.state || null);
 
-  if (!room) {
-    return <h2>Room not found</h2>;
-  }
+  //   if (!room) {
+  //     return <h2>Room not found</h2>;
+  //   }
 
   const handleCellClick = (index) => {
-    console.log("Clicked", index);
+    console.log("Cell clicked:", index);
+
+    const currentPlayer = room.players.find(
+      (player) => player.username === user.username,
+    );
+
+    if (!currentPlayer) return;
+
+    console.log("Current player:", currentPlayer);
+
+    if (room.gameState.currentPlayer !== currentPlayer.symbol) {
+      alert("Not your turn");
+      return;
+    }
+
+    if (room.gameState.winner) {
+      alert("Game Over");
+      return;
+    }
+
+    socket.emit("make-move", {
+      roomCode: room.roomCode,
+      index,
+      symbol: currentPlayer.symbol,
+    });
   };
 
+  const handleRematch = () => {
+    socket.emit("rematch", {
+      roomCode: room.roomCode,
+    });
+  };
+
+  const handleLeaveRoom = () => {
+    socket.emit("leave-room", {
+      roomCode: room.roomCode,
+      username: user.username,
+    });
+
+    navigate("/dashboard");
+  };
+
+  useEffect(() => {
+    if (!room) {
+      socket.emit("get-room", {
+        roomCode,
+      });
+    }
+
+    socket.on("room-data", ({ room }) => {
+      console.log("ROOM DATA RECEIVED");
+      console.log(room);
+      setRoom(room);
+    });
+
+    socket.on("game-updated", ({ room }) => {
+      console.log("GAME UPDATED RECEIVED");
+      console.log(room);
+
+      if (room.gameState.winner && room.gameState.winner !== "DRAW") {
+        winnerAudio.play();
+      }
+
+      setRoom(room);
+    });
+
+    return () => {
+      socket.off("game-updated");
+      socket.off("room-data");
+    };
+  }, []);
+
+  if (!room) {
+    return <h2>Loading room...</h2>;
+  }
+
+  console.log("RENDERING ROOM");
+  console.log(room);
+
+  const showConfetti =
+    room.gameState.winner && room.gameState.winner !== "DRAW";
+
   return (
-    <div>
-      <h1>Room: {room.roomCode}</h1>
-
-      <h2>Status: {room.status}</h2>
-
-      <div>
-        {room.players.map((player) => (
-          <div key={player.socketId}>
-            {player.username} ({player.symbol})
+    <>
+      {showConfetti && (
+        <Confetti
+          width={width}
+          height={height}
+          recycle={false}
+          numberOfPieces={300}
+        />
+      )}
+      <div className="game-room">
+        <div className="room-header">
+          <div>
+            <h1>Tic Tac Arena</h1>
+            <p>Room Code: {room.roomCode}</p>
           </div>
-        ))}
-      </div>
 
-      <Board board={room.gameState.board} onCellClick={handleCellClick} />
-    </div>
+          <div className="room-status">Status: {room.status}</div>
+        </div>
+
+        <div className="players-section">
+          {room.players.map((player) => (
+            <div
+              key={player.socketId}
+              className={`player-card ${
+                room.gameState.currentPlayer === player.symbol
+                  ? "active-player"
+                  : ""
+              }`}
+            >
+              <h3>{player.username}</h3>
+              <span>{player.symbol}</span>
+            </div>
+          ))}
+        </div>
+
+        <div className="game-info">
+          {room.gameState.winner === "DRAW" ? (
+            <h2>🤝 Match Draw</h2>
+          ) : room.gameState.winner ? (
+            <div className="winner-banner">
+              🎉 Winner: {room.gameState.winner}
+            </div>
+          ) : (
+            <h2>Turn : {room.gameState.currentPlayer}</h2>
+          )}
+        </div>
+
+        <div className="board-container">
+          <Board board={room.gameState.board} onCellClick={handleCellClick} />
+        </div>
+
+        {room.gameState.winner && (
+          <div className="game-actions">
+            <button className="rematch-btn" onClick={handleRematch}>
+              🔄 Play Again
+            </button>
+
+            <button className="leave-btn" onClick={handleLeaveRoom}>
+              🚪 Leave Room
+            </button>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
--- server/src/sockets/gameSocket.js
+++ server/src/sockets/gameSocket.js
@@ -1,4 +1,5 @@
 import generateRoomCode from "../utils/generateRoomCode.js";
+import checkWinner from "../utils/checkWinner.js";
 const rooms = {};
 
 const gameSocket = (io) => {
@@ -6,28 +7,26 @@ const gameSocket = (io) => {
     console.log("User Connected", socket.id);
 
     socket.on("create-room", ({ username }) => {
+      console.log("ALL ROOMS AFTER CREATE");
+      console.log(rooms);
       const roomCode = generateRoomCode();
 
       rooms[roomCode] = {
         roomCode,
-        status : "waiting",
+        status: "waiting",
         players: [
           {
             socketId: socket.id,
             username,
-            symbol : "X"
+            symbol: "X",
           },
         ],
 
-        gameState : {
-          board : [
-            "","","",
-            "","","",
-            "","",""
-          ],
-          currentPlayer : "X",
-          winner : null
-        }
+        gameState: {
+          board: ["", "", "", "", "", "", "", "", ""],
+          currentPlayer: "X",
+          winner: null,
+        },
       };
 
       socket.join(roomCode);
@@ -63,7 +62,7 @@ const gameSocket = (io) => {
       room.players.push({
         socketId: socket.id,
         username,
-        symbol:"O"
+        symbol: "O",
       });
 
       if (room.players.length === 2) {
@@ -73,10 +72,122 @@ const gameSocket = (io) => {
       socket.join(roomCode);
 
       io.to(roomCode).emit("room-joined", {
-        room : rooms[roomCode]
+        room: rooms[roomCode],
       });
 
       console.log(`Player joined ${roomCode}`);
+    });
+
+    socket.on("make-move", ({ roomCode, index, symbol }) => {
+      console.log("MOVE RECEIVED");
+      console.log("ALL ROOMS DURING MOVE");
+      console.log(rooms);
+
+      const room = rooms[roomCode];
+
+      if (!room) {
+        console.log("ROOM NOT FOUND");
+        return;
+      }
+
+      if (room.gameState.winner) {
+        console.log("GAME OVER");
+        return;
+      }
+
+      const board = room.gameState.board;
+
+      if (board[index]) {
+        console.log("CELL ALREADY FILLED");
+        return;
+      }
+
+      if (room.gameState.currentPlayer !== symbol) {
+        console.log("WRONG TURN");
+        console.log("Current:", room.gameState.currentPlayer);
+        console.log("Player:", symbol);
+        return;
+      }
+
+      console.log("PASSING VALIDATION");
+
+      board[index] = symbol;
+
+      const winner = checkWinner(board);
+
+      if (winner) {
+        room.gameState.winner = winner;
+      } else if (board.every((cell) => cell !== "")) {
+        room.gameState.winner = "DRAW";
+      } else {
+        room.gameState.currentPlayer = symbol === "X" ? "O" : "X";
+      }
+
+      console.log("EMITTING UPDATE");
+      console.log(room.gameState.board);
+
+      io.to(roomCode).emit("game-updated", {
+        room,
+      });
+    });
+
+    socket.on("get-room", ({ roomCode }) => {
+      console.log("GET ROOM:", roomCode);
+      const room = rooms[roomCode];
+
+      if (!room) {
+        socket.emit("room-data", {
+          room: null,
+        });
+
+        return;
+      }
+
+      socket.join(roomCode);
+
+      socket.emit("room-data", {
+        room,
+      });
+
+      console.log(`${socket.id} rejoined ${roomCode}`);
+    });
+
+    socket.on("rematch", ({ roomCode }) => {
+      const room = rooms[roomCode];
+
+      if (!room) return;
+
+      room.gameState = {
+        board: ["", "", "", "", "", "", "", "", ""],
+
+        currentPlayer: "X",
+
+        winner: null,
+      };
+
+      io.to(roomCode).emit("game-updated", {
+        room,
+      });
+    });
+
+    socket.on("leave-room", ({ roomCode, username }) => {
+      const room = rooms[roomCode];
+
+      if (!room) return;
+
+      socket.leave(roomCode);
+
+      room.players = room.players.filter(
+        (player) => player.username !== username,
+      );
+
+      if (room.players.length === 0) {
+        delete rooms[roomCode];
+
+        console.log(`Deleted room ${roomCode}`);
+      } else {
+        room.status = "waiting";
+      }
     });
 
     socket.on("disconnect", () => {
